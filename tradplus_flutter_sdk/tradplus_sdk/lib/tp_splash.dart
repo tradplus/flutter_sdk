a73282a3d260cad332d11b9a07ec7373e0a14008
Administrator <Brind>
2022-10-19 17:23:21 +0800

    V1.0.3

--- tradplus_flutter_sdk/tradplus_sdk/lib/tp_splash.dart
+++ tradplus_flutter_sdk/tradplus_sdk/lib/tp_splash.dart
@@ -41,6 +41,14 @@ class TPSplash {
     TradplusSdk.channel.invokeMethod('splash_show', arguments);
   }
 
+  ///开发者通过此接口在展示前设置透传信息，透传信息可以在广告展示后的相关回调的adInfo中获取
+  Future<void> setCustomAdInfo(String adUnitId,Map customAdInfo) async {
+    Map arguments = {};
+    arguments['adUnitID'] = adUnitId;
+    arguments['customAdInfo'] = customAdInfo;
+    TradplusSdk.channel.invokeMethod('splash_setCustomAdInfo', arguments);
+  }
+
   ///设置广告Listener：adUnitId 设置后只返回指定广告位相关回调（可选）
   setSplashListener(TPSplashAdListener listener, {String adUnitId = ""}) {
     if (adUnitId.isNotEmpty) {
